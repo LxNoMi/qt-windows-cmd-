05c6d346a9b224930b9d29db581057c17b16ca41
liuxun <1541233570@qq.com>
2019-04-14 17:49:02 +0800

    first commit

---
+++ mainwindow.cpp
@@ -0,0 +1,31 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QProcess>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    this->setMaximumSize(392,180);
+    this->setMinimumSize(392,180);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_submitButton_clicked()
+{
+    QProcess *myProcess = new QProcess();
+    QString startProgram = ui->cmdLineEdit->text();
+    myProcess->start(startProgram.trimmed());
+}
+
+void MainWindow::on_cmdLineEdit_returnPressed()
+{
+    QProcess *myProcess = new QProcess();
+    QString startProgram = ui->cmdLineEdit->text();
+    myProcess->start(startProgram.trimmed());
+}
